6001215db4978ad6a35e6528adf772c50f04c16c
YomiIsayama <scepters1@gmail.com>
2020-04-01 12:00:46 +0800

    officialwebsite

---
+++ pages/index/index.js
@@ -0,0 +1,8 @@
+Page({
+  phone:function(e){
+    wx.makePhoneCall({
+      phoneNumber: '12345678901'
+    
+    })
+  }
+})
---
+++ pages/map/map.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    markers: [{
+      iconPath: "../images/mark.png",
+      id: 0,
+      latitude: 23.3870,
+      longitude: 113.4460,
+      label: {content:'图文楼',}
+    },{
+        iconPath: "../images/mark.png",
+        id: 1,
+        latitude: 23.38300,
+        longitude: 113.44930,        
+        label: { content: '第三饭堂', }
+      }, {
+        iconPath: "../images/mark.png",
+        id: 2,
+        latitude: 23.38300,
+        longitude: 113.44830,        
+        label: { content: '操场', }
+      }]
+  }
+  
+})
